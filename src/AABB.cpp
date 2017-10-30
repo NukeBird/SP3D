e045f2185cb3e764cc48b1cc7a68cb833bc0d33c
NukeBird <nukebird.dev@gmail.com>
2017-10-30 22:54:19 +0300

    Start developing AABB class

---
+++ src/AABB.cpp
@@ -0,0 +1,53 @@
+#include "AABB.h"
+
+namespace SP3D
+{
+	AABB::AABB(const AABB& o)
+	{
+		m_min = o.m_min;
+		m_max = o.m_max;
+	}
+
+	AABB::AABB(const glm::vec3& a, const glm::vec3& b)
+	{
+		m_min = glm::min(a, b);
+		m_max = glm::max(a, b);
+	}
+
+	void AABB::extend(const glm::vec3& point)
+	{
+		m_min = glm::min(m_min, point);
+		m_max = glm::max(m_max, point);
+	}
+
+	void AABB::extend(const AABB& o)
+	{
+		extend(o.m_min);
+		extend(o.m_max);
+	}
+
+	glm::vec3 AABB::get_min() const
+	{
+		return m_min;
+	}
+
+	glm::vec3 AABB::get_max() const
+	{
+		return m_max;
+	}
+
+	glm::vec3 AABB::get_diagonal() const
+	{
+		return m_max - m_min;
+	}
+
+	float AABB::get_diagonal_length() const
+	{
+		return glm::length(get_diagonal());
+	}
+
+	glm::vec3 AABB::get_center() const
+	{
+		return (m_min + m_max) / 2.0f;
+	}
+};
---
+++ src/AABB.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <glm/glm.hpp>
+
+namespace SP3D
+{
+	//Axis-aligned bounding box (AABB)
+	class AABB final
+	{
+	public:
+		AABB(const AABB& o);
+
+		//Builds AABB that contain points a and b
+		AABB(const glm::vec3& a, const glm::vec3& b);
+
+		//Includes point to AABB volume
+		void extend(const glm::vec3& point);
+
+		///TODO: Write a description.
+		///My English sucks as usual
+		void extend(const AABB& o);
+
+		//Get AABB's min point
+		glm::vec3 get_min() const;
+
+		//Get AABB's max point
+		glm::vec3 get_max() const;
+
+		//Get AABB's center
+		glm::vec3 get_center() const;
+
+		//Get AABB's diagonal
+		glm::vec3 get_diagonal() const;
+
+		//Get AABB's diagonal length
+		float get_diagonal_length() const;
+	private:
+		glm::vec3 m_min; //min point
+		glm::vec3 m_max; //max point
+	};
+};
